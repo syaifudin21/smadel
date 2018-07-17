6b13053cc7e19f2d86f3cc5aedd23974c02ca9f9
Saifudin <saifudin@dikanetwork.com>
2018-07-17 11:22:44 +0700

    update dari lokal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel laravel
                 </div>
 
                 <div class="links">
