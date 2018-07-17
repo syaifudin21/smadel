d0a47641ac5467b064d9f3d24ca8c042c519e722
Saifudin <saifudin@dikanetwork.com>
2018-07-17 12:13:20 +0700

    tinggal sistem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use App\Models\Profil_sekolah;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::share('profil', Profil_sekolah::find(1));
     }
 
     /**
