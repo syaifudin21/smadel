6699c1c00bc3806a49d50d79cf5b4f322a5ef0bb
Saifudin <saifudin@dikanetwork.com>
2018-07-17 12:23:58 +0700

    coba lagi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+         Schema::defaultStringLength(191);
         // View::share('profil', Profil_sekolah::find(1));
     }
 
