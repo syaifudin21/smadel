fb2b00e4d11ffcbecb0724314326d08513240360
Saifudin <saifudin@dikanetwork.com>
2018-07-20 13:03:12 +0700

    album bug

--- laravel/resources/views/front/album.blade.php
+++ laravel/resources/views/front/album.blade.php
@@ -30,11 +30,14 @@
     <center>
       <?php 
         if ($album->status_user == 'pengurus') {
-            $nama = App\Models\Pengurus::find($album->id_user)->nama;
+            $dd = App\Models\Pengurus::find($album->id_user);
+                $nama = (!empty($dd))? $dd->nama : 'NN';
         }elseif ($album->status_user == 'guru') {
-            $nama = App\Models\Pengajars::find($album->id_user)->nama;
+            $dd = App\Models\Pengajars::find($album->id_user);
+                $nama = (!empty($dd))? $dd->nama : 'NN';
         }elseif ($album->status_user == 'siswa') {
-            $nama = App\Models\Siswas::find($album->id_user)->nama;
+            $dd = App\Models\Siswas::find($album->id_user);
+                $nama = (!empty($dd))? $dd->nama : 'NN';
         }else{
             $nama = 'NN';
         }
