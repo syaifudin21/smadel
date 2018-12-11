81253a5ba0311f8b1cfc62d58a187d0277f2d2a7
Saifudin <saifudin@dikanetwork.com>
2018-12-11 14:07:25 +0700

    tertinggal

--- laravel/app/Http/Controllers/Android/Siswa/SiswaController.php
+++ laravel/app/Http/Controllers/Android/Siswa/SiswaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Android\Siswa;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Siswa;
 use App\Models\Profil_siswa;
 
